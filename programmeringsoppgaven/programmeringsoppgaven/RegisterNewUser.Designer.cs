75390bbd855a32f3288f363217e995df9724e356
eivindjs <eivind.skreddernes@gmail.com>
2014-04-22 19:14:26 +0200

    Tord fikset feil

---
+++ programmeringsoppgaven/programmeringsoppgaven/RegisterNewUser.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace projectcsharp
+{
+    partial class RegisterNewUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUsername.Location = new System.Drawing.Point(133, 50);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(100, 22);
+            this.tbUsername.TabIndex = 0;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(133, 91);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(100, 22);
+            this.tbPassword.TabIndex = 1;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(75, 135);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(129, 23);
+            this.btnRegister.TabIndex = 2;
+            this.btnRegister.Text = "Register new account";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Create username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Create password:";
+            // 
+            // RegisterNewUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::projectcsharp.Properties.Resources.loginbackground;
+            this.ClientSize = new System.Drawing.Size(386, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Name = "RegisterNewUser";
+            this.Text = "RegisterNewUser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ programmeringsoppgaven/programmeringsoppgaven/RegisterNewUser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projectcsharp
+{
+    public partial class RegisterNewUser : Form
+    {
+        //deklarerer variabler
+        private DBConnect db = new DBConnect();
+        private DataTable table = new DataTable();
+        private string username;
+        private string password;
+        private string passwordIn;
+        private String query;
+
+        public RegisterNewUser()
+        {
+            InitializeComponent();
+        }
+
+        //registrerer ny bruker(legges i database) ved trykk
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            username = tbUsername.Text;
+            passwordIn = tbPassword.Text;
+            password = Encryption.Encrypt(passwordIn);
+            query = String.Format("INSERT INTO User (username, password) VALUES('{0}', '{1}')", username, password);
+
+            //tar i mot og behandler exceptions
+            try
+            {
+                db.InsertDeleteUpdate(query);
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Feil oppsto ved INSERT med melding: " + ex.Message);
+            }
+            finally
+            {
+                this.Close();
+            }
+
+        }
+    }
+}
