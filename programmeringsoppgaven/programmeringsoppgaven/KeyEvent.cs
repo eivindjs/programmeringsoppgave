4029e5ff5d17f1a40ecedf30b05ac847faaf1028
eivindjs <eivind.skreddernes@gmail.com>
2014-05-18 16:34:37 +0200

    Kommentert KeyEvent

--- programmeringsoppgaven/programmeringsoppgaven/KeyEvent.cs
+++ programmeringsoppgaven/programmeringsoppgaven/KeyEvent.cs
@@ -10,49 +10,57 @@ namespace projectcsharp
 {
     /// <summary>
     /// Klasse for når du trykker på piltatastene. Klassen har en struct som har tre egenskaper(Key, isPressed, isToggled). 
-    /// Disse eiendommene er satt når GetKeyState blir kalt.
+    /// Disse eiendommene er satt når GetKeyState blir kalt. Key egenskapen er for selve tasten, hvilken tast som blir trykket.
+    /// isPressed er om den er trykket(true/false) og isToggled er om den er av eller på(true/false), men det gjelder mest for capslock, numlock osv. 
+    /// Det spørs om du skal bruke caps eller ikke.
     /// http://sanity-free.org/17/obtaining_key_state_info_in_dotnet_csharp_getkeystate_implementation.html
     /// </summary>
     public class KeyEvent
     {
         private KeyEvent() { }
-        [DllImport("user32")]
-        private static extern short GetKeyState(int vKey);
 
-        public static KeyStateInfo GetKeyState(Keys key)
+        [DllImport("user32")]//forteller kompilatoren hvilken DLL den skal referer til
+        private static extern short GetKeyState(int vKey); //Kaller på metode som eksisterer i en DLL
+
+        /// <summary>
+        /// Kontruktør
+        /// </summary>
+        /// <param name="key">Tast</param>
+        /// <returns>Tasten, om den er trykket, av/på</returns>
+        public static KeyEventInfo GetKeyState(Keys key)
         {
             short keyState = GetKeyState((int)key);
             byte[] bits = BitConverter.GetBytes(keyState);
             bool toggled = bits[0] > 0, pressed = bits[1] > 0;
-            return new KeyStateInfo(key, pressed, toggled);
+            return new KeyEventInfo(key, pressed, toggled);
         }
     }
 
     /// <summary>
     /// Struct klasse
     /// </summary>
-    public struct KeyStateInfo
+    public struct KeyEventInfo
     {
-        Keys _key;
-        bool _isPressed, _isToggled;
+        Keys _key; //tasten
+        bool _isPressed, _isToggled; //boolske variabler for taste trykk og av/på
 
         /// <summary>
         /// Konstruktør 
         /// </summary>
         /// <param name="key">Key/Piltast</param>
         /// <param name="ispressed">Om den er trykket</param>
-        /// <param name="istoggled">Om den er på/av</param>
-        public KeyStateInfo(Keys key, bool ispressed, bool istoggled)
+        /// <param name="istoggled">Om den er på/av(gjelder f.eks caps lock osv)</param>
+        public KeyEventInfo(Keys key, bool ispressed, bool istoggled)
         {
             _key = key;
             _isPressed = ispressed;
             _isToggled = istoggled;
         }
-        public static KeyStateInfo Default
+        public static KeyEventInfo Default
         {
             get
             {
-                return new KeyStateInfo(Keys.None,false,false);
+                return new KeyEventInfo(Keys.None, false, false);
             }
         }
         public Keys Key
